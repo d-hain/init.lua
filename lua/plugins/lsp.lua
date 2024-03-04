4200321a240b9a5fae5b0f05c0236a07bb064d86
David Hain <d.hain@gmx.at>
2024-03-04 15:55:33 +0100

    feat: added function to rename without lsp using treesitter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -104,13 +104,64 @@ return {
             },
         },
         config = function(_, _)
+            local ts_utils = require("nvim-treesitter.ts_utils")
+            local locals = require("nvim-treesitter.locals")
+            local utils = require("nvim-treesitter.utils")
+
+            local get_node_text = vim.treesitter.get_node_text or vim.treesitter.query.get_node_text
+            local M = {}
+
+            -- NOTE: yoinked from https://github.com/nvim-treesitter/nvim-treesitter-refactor/tree/65ad2eca822dfaec2a3603119ec3cc8826a7859e
+            function M.smart_rename(bufnr)
+                bufnr = bufnr or vim.api.nvim_get_current_buf()
+                local node_at_point = ts_utils.get_node_at_cursor()
+                local function complete_rename(new_name)
+                    -- Empty name cancels the interaction or ESC
+                    if not new_name or #new_name < 1 then
+                        return
+                    end
+
+                    local definition, scope = locals.find_definition(node_at_point, bufnr)
+                    local nodes_to_rename = {}
+                    nodes_to_rename[node_at_point:id()] = node_at_point
+                    nodes_to_rename[definition:id()] = definition
+
+                    for _, n in ipairs(locals.find_usages(definition, scope, bufnr)) do
+                        nodes_to_rename[n:id()] = n
+                    end
+
+                    local edits = {}
+
+                    for _, node in pairs(nodes_to_rename) do
+                        local lsp_range = ts_utils.node_to_lsp_range(node)
+                        local text_edit = { range = lsp_range, newText = new_name }
+                        table.insert(edits, text_edit)
+                    end
+                    vim.lsp.util.apply_text_edits(edits, bufnr, "utf-8")
+                end
+
+                if not node_at_point then
+                    utils.print_warning "No node to rename!"
+                    return
+                end
+
+                local node_text = get_node_text(node_at_point, bufnr)
+                local input = { prompt = "New name: ", default = node_text or "" }
+                if not vim.ui.input then
+                    local new_name = vim.fn.input(input.prompt, input.default)
+                    complete_rename(new_name)
+                else
+                    vim.ui.input(input, complete_rename)
+                end
+            end
+
             local lspconfig = require("lspconfig")
 
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
             capabilities.offsetEncoding = { "utf-16" }
             capabilities.textDocument.completion.completionItem.snippetSupport = true
-            -- yanked from https://github.com/jdah/dotfiles and I have no idea what it does
+            -- NOTE: yanked from https://github.com/jdah/dotfiles and I have no idea what it does
             capabilities.textDocument.completion.completionItem.resolveSupport = {
                 properties = { "documentation", "detail", "additionalTextEdits" },
             }
@@ -164,16 +215,17 @@ return {
                 end,
             }
 
-            -- Global mappings.
+            -- Global mappings
             vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float)
             vim.keymap.set("n", "<leader>nd", vim.diagnostic.goto_next)
             vim.keymap.set("n", "<leader>pd", vim.diagnostic.goto_prev)
 
-            -- LSP Buffer mappings.
+            -- LSP Buffer mappings
             vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-                callback = function(ev)
-                    local opts = { buffer = ev.buf }
+                callback = function(args)
+                    local opts = { buffer = args.buf }
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
 
                     vim.lsp.handlers["textDocument/signature_help"] = vim.lsp.with(
                         vim.lsp.handlers.signature_help, {
@@ -191,7 +243,13 @@ return {
                         require("lsp_signature").toggle_float_win()
                     end, opts)
                     vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<leader>rn", function()
+                        if client.server_capabilities.renameProvider then
+                            vim.lsp.buf.rename()
+                        else
+                            M.smart_rename(args.buf)
+                        end
+                    end, opts)
                     vim.keymap.set("n", "<leader>f", function()
                         if vim.bo.filetype == "json" then
                             vim.cmd("%!jq '.'")
