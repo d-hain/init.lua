406366bc3f55071aa50af1e7c8467a1c1fcc8aac
David Hain <d.hain@gmx.at>
2024-05-22 21:00:10 +0200

    feat: added shortcut to move to specific line and column in a buffer

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Setup lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,5 +22,10 @@ require("lazy").setup("plugins", {
     }
 })
 require("config.remaps")
+
+-- Shortcut to get to line and column
+vim.api.nvim_create_user_command("Lc", function(opts)
+    vim.api.nvim_win_set_cursor(0, { tonumber(opts.fargs[1]), tonumber(opts.fargs[2]) })
+end, { nargs = "+" })
 
 print("The One Piece ... THE ONE PIECE IS REEAAALLLL!!!")
