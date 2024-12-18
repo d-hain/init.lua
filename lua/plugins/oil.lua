77ccb7f2fa286a24b193fe247117a6cacf230e0e
David Hain <d.hain@gmx.at>
2024-12-18 20:37:47 +0100

    feat: oil: show hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,7 +3,11 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
 
     config = function(_, _)
-        require("oil").setup()
+        require("oil").setup {
+            view_options = {
+                show_hidden = true,
+            }
+        }
 
         vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
     end
