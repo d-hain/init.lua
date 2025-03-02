d6f5de90b95aea3ce6c363463ba56aae4d16bde2
David Hain <d.hain@gmx.at>
2025-03-02 15:09:01 +0100

    feat: move from mardown.nvim to markview.nvim

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,6 @@
+return {
+    "OXY2DEV/markview.nvim",
+    -- Needs "markdown" and "markdown_inline" treesitter parsers
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    lazy = false,
+}
