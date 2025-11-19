03e93c44fbeb3144292e58aba9d031824a17f793
David Hain <d.hain@gmx.at>
2025-11-19 18:30:11 +0100

    feat: add finding keybind i am used to from Visual Studio

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
     config = function(_, _)
         local builtin = require("telescope.builtin")
 
-        vim.keymap.set("n", "<leader>pf", function ()
+        vim.keymap.set("n", "<leader>pf", function()
             builtin.find_files {
                 hidden = true,
                 no_ignore = true,
@@ -12,5 +12,20 @@ return {
             }
         end, {})
         vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+
+        -- Visual Studio - hell yeah
+        vim.keymap.set("n", "<C-S-f>", function()
+            local word = vim.fn.expand("<cword>")
+            builtin.live_grep {
+                default_text = word
+            }
+        end)
+        vim.keymap.set("v", "<C-S-f>", function()
+            vim.cmd('normal! "zy')
+            local selection = vim.fn.getreg("z")
+            builtin.live_grep {
+                default_text = selection
+            }
+        end)
     end,
 }
